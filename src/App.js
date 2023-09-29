8e27cbf975b86b699717f18c24ce8046f0e488f5
Curtis Kettler <curtiskettler@mindsetconsulting.com>
2023-09-29 14:39:21 -0700

    bad version of application created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import foods from "./list";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div style={{ textAlign: "center" }}>
+        <h1
+          style={{
+            color: "orange",
+            fontSize: "5rem",
+            fontFamily: "Impact, sans-serif",
+            transform: "rotate(-5deg)",
+            textShadow: "2px 2px 5px rgba(0, 0, 0, 0.3)",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          The Best App Ever
+        </h1>
+        <h2>{count}</h2>
+        <button
+          style={{
+            backgroundColor: "green",
+            color: "white",
+            border: "none",
+            padding: "10px 20px",
+            margin: "10px",
+            borderRadius: "50%",
+            boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.3)",
+          }}
+          onClick={() => setCount(count - 1)}
+        >
+          -
+        </button>
+
+        <button
+          style={{
+            backgroundColor: "blue",
+            color: "white",
+            border: "none",
+            padding: "10px 20px",
+            margin: "10px",
+            borderRadius: "50%",
+            boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.3)",
+          }}
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
+
+        <button
+          style={{
+            backgroundColor: "red",
+            color: "white",
+            border: "none",
+            padding: "10px 20px",
+            margin: "10px",
+            borderRadius: "50%",
+            boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.3)",
+          }}
+          onClick={() => {
+            setCount(count - 1);
+            setCount(count - 1);
+          }}
+        >
+          -2
+        </button>
+
+        <button
+          style={{
+            backgroundColor: "orange",
+            color: "white",
+            border: "none",
+            padding: "10px 20px",
+            margin: "10px",
+            borderRadius: "50%",
+            boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.3)",
+          }}
+          onClick={() => {
+            setCount(count + 1);
+            setCount(count + 1);
+          }}
+        >
+          +2
+        </button>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <button
+          style={{
+            backgroundColor: "grey",
+            color: "white",
+            border: "none",
+            padding: "10px 20px",
+            margin: "10px",
+          }}
+          onClick={() => setCount(0)}
+        >
+          Reset
+        </button>
+      </div>
+      <div>
+        <h2 style={{ textAlign: "center" }}>Food List</h2>
+        <table style={{ margin: "auto" }}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Country</th>
+              <th>Flavor Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            {foods.map((food) => (
+              <tr key={food.id}>
+                <td>{food.foNa}</td>
+                <td>{food.foCo}</td>
+                <td>{food.foRa}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/list.js
@@ -0,0 +1,64 @@
+let foods = [
+    {
+      "id": 1,
+      "foNa": "Pizza",
+      "foCo": "Italy",
+      "foRa": 9.5
+    },
+    {
+      "id": 2,
+      "foNa": "Sushi",
+      "foCo": "Japan",
+      "foRa": 8.5
+    },
+    {
+      "id": 3,
+      "foNa": "Tacos",
+      "foCo": "Mexico",
+      "foRa": 9.0
+    },
+    {
+      "id": 4,
+      "foNa": "Pad Thai",
+      "foCo": "Thailand",
+      "foRa": 8.0
+    },
+    {
+      "id": 5,
+      "foNa": "Burger",
+      "foCo": "United States",
+      "foRa": 8.5
+    },
+    {
+      "id": 6,
+      "foNa": "Pasta",
+      "foCo": "Italy",
+      "foRa": 9.0
+    },
+    {
+      "id": 7,
+      "foNa": "Curry",
+      "foCo": "India",
+      "foRa": 9.0
+    },
+    {
+      "id": 8,
+      "foNa": "Pho",
+      "foCo": "Vietnam",
+      "foRa": 8.5
+    },
+    {
+      "id": 9,
+      "foNa": "Falafel",
+      "foCo": "Egypt",
+      "foRa": 8.0
+    },
+    {
+      "id": 10,
+      "foNa": "Schnitzel",
+      "foCo": "Germany",
+      "foRa": 8.0
+    }
+  ]
+
+  export default foods;
